456bf4e3d0bc230be7605b5530e52fad5be1f1fe
Ted Miller <ted.miller92@gmail.com>
2021-06-28 09:27:44 -0700

    First test commit

---
+++ nodetest.js
@@ -0,0 +1,22 @@
+const http = require("http");
+const fs = require("fs");
+http.createServer((req,res) => {
+    var path = req.url.toLowerCase();
+    switch(path) {
+        case '/':
+            fs.readFile("home.html", (err, data) => {
+             if (err) return console.error(err);
+                res.writeHead(200, {'Content-Type': 'text/html'});
+             res.end(data.toString());
+            });
+            break;
+        case '/about':
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('About page');
+            break;
+        default:
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.end('Not found');
+            break;
+    }
+}).listen(process.env.PORT || 3000);
