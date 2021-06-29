103f05cf8fa089f9f53dd6bcc87d18606b5d16ab
Ted Miller <ted.miller92@gmail.com>
2021-06-29 12:15:02 -0700

    Commit for Assignment 1

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require("http");
+http.createServer((req,res) => {
+	var path = req.url.toLowerCase();
+	switch(path) {
+		case '/':
+			res.writeHead(200, {'Content-Type' : 'text/plain'});
+			res.end('Home Page');
+			break;
+		case '/about':
+			res.writeHead(200, {'Content-Type' : 'text/plain'});
+			res.end('About Page');
+			break;
+		default:
+			res.writeHead(404, {'Content-Type' : 'text/plain'});
+			res.end('Not Found');
+			break;
+	}
+}).listen(process.env.PORT || 3000);
