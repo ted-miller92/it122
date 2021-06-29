581d1e4174f84aa3d394d0d585c5c7d99e4cce2f
Ted Miller <ted.miller92@gmail.com>
2021-06-29 12:58:46 -0700

    Assignent 1 - Added more content to About page

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ http.createServer((req,res) => {
 			break;
 		case '/about':
 			res.writeHead(200, {'Content-Type' : 'text/plain'});
-			res.end('About Page');
+			res.write('About Page\n');
+			res.write('This is my first node.js page. The second, if you count the Home Page.\n');
+			res.end('I started learning web authoring and development in the Summer of 2020, when I found myself with no income and a lot of time on my hands...\nSince then, I\'ve learned HTML, CSS, and some rudimentary Javascript and Python.');
 			break;
 		default:
 			res.writeHead(404, {'Content-Type' : 'text/plain'});
