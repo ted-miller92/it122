6270ae227e5accfb05e78b3a9baa3abf6bca1a22
Ted Miller <ted.miller92@gmail.com>
2021-07-17 22:05:41 -0700

    Home route now shows all bands with bands.getAll()

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
-// const http = require("http");
 import http from 'http';
 import fs from 'fs';
-import * as bands from './data.js'
-import { parse } from "querystring"
+import * as bands from './data.js';
+import { parse } from "querystring";
+'use strict'
+// import express from "express";
 
+// var app = express();
+
+// //Setting up handlebars view engine
+// import exphbs from "express-handlebars";
+// app.engine("handlebars", exphbs({defaultLayout: false}));
+// app.set("view engine", "handlebars");
+
+// app.set("port", process.env.PORT || 3000);
+// app.use(express.static("public"));		//Sets location for static files
+// app.use(express.urlencoded({extended: true}));				//parse url-encoded bodies
+
+
+// // app.get('/', (req, res) => {
+// // 	res.type("text/html");
+// // 	res.sendFile('./public/home.html');
+// // });
+
+// // or, using handlebars...
+// app.get('/', (req,res) => {
+// 	res.render('home');
+// })
+
+// app.get('/about', (req, res) => {
+// 	res.type("text/plain");
+// 	res.send("About Page\nThis is my first node.js page. The second, if you count the Home Page.\nI started learning web authoring and development in the Summer of 2020, when I found myself with no income and a lot of time on my hands...\nSince then, I\'ve learned HTML, CSS, and some rudimentary Javascript and Python.");
+// });
+
+// app.get('/detail', (req, res) => {
+// 	let result = bands.getItem(req.query.name);
+// 	res.render('detail', {name: req.query.name, result: result});
+// })
+
+// app.use((req, res) => {
+// 	res.type("text/plain");
+// 	res.status(404);
+// 	res.send('404 - Not Found');
+// });
+
+// app.listen(app.get("port"), () => {
+// 	console.log(`Express App Started on port ${app.get('port')}`);
+// });
 
 http.createServer((req,res) => {
 	
-
 	let url = req.url.split("?");  // separate route from query string
 	let query = parse(url[1]); // convert query string to a JS object
 	console.log(query.name);
@@ -17,7 +58,7 @@ http.createServer((req,res) => {
 	switch(path) {
 		case '/':
 			res.writeHead(200, {'Content-Type' : 'text/plain'});
-			res.end(JSON.stringify(bands));
+			res.end(JSON.stringify(bands.getAll()));
 			break;
 		case '/about':
 			res.writeHead(200, {'Content-Type' : 'text/plain'});
