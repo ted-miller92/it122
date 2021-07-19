fd1e4fbb3d7bba9f5d536bcfab13b2bf23ccfd80
Ted Miller <ted.miller92@gmail.com>
2021-07-19 11:58:49 -0700

    Commit for Assignment 3 - Express Yourself

--- index.js
+++ index.js
@@ -3,84 +3,92 @@ import fs from 'fs';
 import * as bands from './data.js';
 import { parse } from "querystring";
 'use strict'
-// import express from "express";
+import express from "express";
 
-// var app = express();
+var app = express();
 
-// //Setting up handlebars view engine
-// import exphbs from "express-handlebars";
-// app.engine("handlebars", exphbs({defaultLayout: false}));
-// app.set("view engine", "handlebars");
+//Setting up handlebars view engine
+import exphbs from "express-handlebars";
+app.engine("handlebars", exphbs({defaultLayout: false}));
+app.set("view engine", "handlebars");
 
-// app.set("port", process.env.PORT || 3000);
-// app.use(express.static("public"));		//Sets location for static files
-// app.use(express.urlencoded({extended: true}));				//parse url-encoded bodies
+// listen on default or port 3000
+app.set("port", process.env.PORT || 3000);
+
+//Set location for static files
+app.use(express.static("public"));
+
+//parse url-encoded bodies
+app.use(express.urlencoded({extended: true}));
 
 
-// // app.get('/', (req, res) => {
-// // 	res.type("text/html");
-// // 	res.sendFile('./public/home.html');
-// // });
-
-// // or, using handlebars...
-// app.get('/', (req,res) => {
-// 	res.render('home');
-// })
-
-// app.get('/about', (req, res) => {
-// 	res.type("text/plain");
-// 	res.send("About Page\nThis is my first node.js page. The second, if you count the Home Page.\nI started learning web authoring and development in the Summer of 2020, when I found myself with no income and a lot of time on my hands...\nSince then, I\'ve learned HTML, CSS, and some rudimentary Javascript and Python.");
+// app.get('/', (req, res) => {
+// 	res.type("text/html");
+// 	res.sendFile('./public/home.html');
 // });
 
-// app.get('/detail', (req, res) => {
-// 	let result = bands.getItem(req.query.name);
-// 	res.render('detail', {name: req.query.name, result: result});
-// })
+// or, using handlebars...
+app.get('/', (req,res) => {
+	let result = bands.getAll();
+	res.render('home', {name: req.query.name, result, result});
+})
 
-// app.use((req, res) => {
-// 	res.type("text/plain");
-// 	res.status(404);
-// 	res.send('404 - Not Found');
-// });
+app.get('/about', (req, res) => {
+	res.type("text/plain");
+	res.send("About Page\nThis is my first node.js page. The second, if you count the Home Page.\nI started learning web authoring and development in the Summer of 2020, when I found myself with no income and a lot of time on my hands...\nSince then, I\'ve learned HTML, CSS, and some rudimentary Javascript and Python.");
+});
 
-// app.listen(app.get("port"), () => {
-// 	console.log(`Express App Started on port ${app.get('port')}`);
-// });
+app.get('/detail', (req, res) => {
+	let result = bands.getItem(req.query.name);
+	res.render('detail', {name: req.query.name, result: result});
+})
 
-http.createServer((req,res) => {
+app.use((req, res) => {
+	res.type("text/plain");
+	res.status(404);
+	res.send('404 - Not Found');
+});
+
+app.listen(app.get("port"), () => {
+	console.log(`Express App Started on port ${app.get('port')}`);
+});
+
+
+//Strictly Node from here to end --------------------
+// http.createServer((req,res) => {
 	
-	let url = req.url.split("?");  // separate route from query string
-	let query = parse(url[1]); // convert query string to a JS object
-	console.log(query.name);
+// 	let url = req.url.split("?");  // separate route from query string
+// 	let query = parse(url[1]); // convert query string to a JS object
+// 	console.log(query.name);
 
-	let path = url[0].toLowerCase();
+// 	let path = url[0].toLowerCase();
 
-	switch(path) {
-		case '/':
-			res.writeHead(200, {'Content-Type' : 'text/plain'});
-			res.end(JSON.stringify(bands.getAll()));
-			break;
-		case '/about':
-			res.writeHead(200, {'Content-Type' : 'text/plain'});
-			res.write('About Page\n');
-			res.write('This is my first node.js page. The second, if you count the Home Page.\n');
-			res.end('I started learning web authoring and development in the Summer of 2020, when I found myself with no income and a lot of time on my hands...\nSince then, I\'ve learned HTML, CSS, and some rudimentary Javascript and Python.');
-			break;
-		case '/detail':
-			let bandName = bands.getItem(query.name);
+// 	switch(path) {
+// 		case '/':
+// 			res.writeHead(200, {'Content-Type' : 'text/plain'});
+// 			res.end(JSON.stringify(bands.getAll()));
+// 			break;
+// 		case '/about':
+// 			res.writeHead(200, {'Content-Type' : 'text/plain'});
+// 			res.write('About Page\n');
+// 			res.write('This is my first node.js page. The second, if you count the Home Page.\n');
+// 			res.end('I started learning web authoring and development in the Summer of 2020, when I found myself with no income and a lot of time on my hands...\nSince then, I\'ve learned HTML, CSS, and some rudimentary Javascript and Python.');
+// 			break;
+// 		case '/detail':
+// 			let bandName = bands.getItem(query.name);
 			
-			let results;
-			if (bandName){
-				results = JSON.stringify(bandName);
-			} else {
-				results = "Does Not Exist";
-			}
-			res.writeHead(200, {'Content-Type' : 'text/plain'});
-			res.end(results);
-			break;
-		default:
-			res.writeHead(404, {'Content-Type' : 'text/plain'});
-			res.end('Not Found');
-			break;
-	}
-}).listen(process.env.PORT || 3000);
+// 			let results;
+// 			if (bandName){
+// 				results = JSON.stringify(bandName);
+// 			} else {
+// 				results = "Does Not Exist";
+// 			}
+// 			res.writeHead(200, {'Content-Type' : 'text/plain'});
+// 			res.end(results);
+// 			break;
+// 		default:
+// 			res.writeHead(404, {'Content-Type' : 'text/plain'});
+// 			res.end('Not Found');
+// 			break;
+// 	}
+// }).listen(process.env.PORT || 3000);
